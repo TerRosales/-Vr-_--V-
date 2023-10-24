c698b8ab00c36755c51589f372d518f739a27cd3
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-10-24 16:49:50 -0400

    go happy

--- js/main.js
+++ js/main.js
@@ -84,19 +84,139 @@ containers.forEach(containers => {
 })
 
 
-// Vanilla tilt Uses
-const tiltElement = document.querySelectorAll('.project-card--details');
+const tiltElements = document.querySelectorAll('.project-card--details');
 
-VanillaTilt.init(tiltElement);
-
-
-VanillaTilt.init(tiltElement, {
-    max: 25,  // Maximum tilt rotation (in degrees)
-    speed: 100, // Tilt speed
-    glare: true, // Enable glare effect
-    "max-glare": 0.3, // Max glare opacity
+tiltElements.forEach((element) => {
+VanillaTilt.init(element, {
+    max: 25,
+    speed: 100,
+    glare: true,
+    "max-glare": 0.3,
+    onTilt: function (tilt) {
+    const shadowIntensity = Math.abs(tilt.tiltX / 20);
+    
+    // Add an inner shadow
+    const innerBoxShadow = `inset 10px 10px 10px rgba(127, 255, 212, 1)`;
+    
+    // Add an outer shadow
+    const outerBoxShadow = `10px 10px 10px 10px rgba(250, 250, 250, 1)`;
+    
+    element.style.boxShadow = `${innerBoxShadow}, ${outerBoxShadow}`;
+    },
+});
 });
 
+// const knowMoreButton = document.querySelector(".project-card--button-1");
+// const projectCardDetails = document.querySelector(".project-card--details");
 
+// // Add a click event listener to the button
+// knowMoreButton.addEventListener("click", () => {
+//     // Change background, h4 text, and p text
+//     projectCardDetails.style.backgroundImage = "url('your-new-image-url.jpg')";
+//     projectCardDetails.style.backgroundSize = "cover";
+//     projectCardDetails.querySelector("h4").textContent = "Project 11";
+//     projectCardDetails.querySelector("p").textContent = "GitPages";
+// });
 
+const projectCardDetails = document.querySelector(".project-card--details");
+
+const knowMoreButton = document.querySelector(".project-card--button-1");
+const knowMoreButton2 = document.querySelector(".project-card--button-1");
+const knowMoreButton3 = document.querySelector(".project-card--button-1");
+
+function clearProjectCardDetails() {
+    // Get the project-card-details element
+    const projectCardDetails = document.querySelectorAll(".project-card--details");
+
+    // Check if the link button already exists and remove it
+    const linkButton = projectCardDetails.querySelector("#link-button");
+    if (linkButton) {
+        projectCardDetails.removeChild(linkButton);
+    }
+
+    // Clear any other content in project-card-details
+    projectCardDetails.innerHTML = "";
+}
+
+// Add a click event listener to the "Know More" button
+knowMoreButton.addEventListener("click", () => {
+    // Clear existing content
+    clearProjectCardDetails();
+    // Change background, h4 text, and p text
+    projectCardDetails.style.backgroundImage = "url('your-new-image-url.jpg')";
+    projectCardDetails.style.backgroundSize = "cover";
+    projectCardDetails.querySelector("h4").textContent = "New Title";
+    projectCardDetails.querySelector("p").textContent = "New text goes here.";
+    
+    // Check if the link button already exists
+    let linkButton = projectCardDetails.querySelector("#link-button");
+    
+    if (!linkButton) {
+        // If it doesn't exist, create and append the link button
+        linkButton = document.createElement("button");
+        linkButton.textContent = "Visit Page";
+        linkButton.id = "link-button"; // Set an ID for the button
+        projectCardDetails.appendChild(linkButton);
+        
+        // Add a click event listener to the link button
+        linkButton.addEventListener("click", () => {
+            // Redirect to the desired page when the link button is clicked
+            window.location.href = "your-target-page.html";
+        });
+    }
+});
+
+knowMoreButton2.addEventListener("click", () => {
+    // Clear existing content
+    clearProjectCardDetails();
+    // Change background, h4 text, and p text
+    projectCardDetails.style.backgroundImage = "url('your-new-image-url.jpg')";
+    projectCardDetails.style.backgroundSize = "cover";
+    projectCardDetails.querySelector("h4").textContent = "New Title";
+    projectCardDetails.querySelector("p").textContent = "New text goes here.";
+    
+    // Check if the link button already exists
+    let linkButton = projectCardDetails.querySelector("#link-button");
+    
+    if (!linkButton) {
+        // If it doesn't exist, create and append the link button
+        linkButton = document.createElement("button");
+        linkButton.textContent = "2";
+        linkButton.id = "link-button"; // Set an ID for the button
+        projectCardDetails.appendChild(linkButton);
+        
+        // Add a click event listener to the link button
+        linkButton.addEventListener("click", () => {
+            // Redirect to the desired page when the link button is clicked
+            window.location.href = "your-target-page.html";
+        });
+    }
+});
+
+knowMoreButton3.addEventListener("click", () => {
+    // Clear existing content
+    clearProjectCardDetails();
+    // Change background, h4 text, and p text
+    projectCardDetails.style.backgroundImage = "url('your-new-image-url.jpg')";
+    projectCardDetails.style.backgroundSize = "cover";
+    projectCardDetails.querySelector("h4").textContent = "New Title";
+    projectCardDetails.querySelector("p").textContent = "New text goes here.";
+    
+    // Check if the link button already exists
+    let linkButton = projectCardDetails.querySelector("#link-button");
+    
+    if (!linkButton) {
+        // If it doesn't exist, create and append the link button
+        linkButton = document.createElement("button");
+        linkButton.textContent = "3";
+        linkButton.id = "link-button"; // Set an ID for the button
+        projectCardDetails.appendChild(linkButton);
+        
+        // Add a click event listener to the link button
+        linkButton.addEventListener("click", () => {
+            // Redirect to the desired page when the link button is clicked
+            window.open("https://terrosales.github.io/project-twelve/", "_blank");
+        });
+    }
+});
 
